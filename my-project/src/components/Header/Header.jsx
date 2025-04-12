1344fbe74ba386c1fd955d16e59da203c6b9e1dc
Hikma Ahmed <hikmahmed497@gmail.com>
2025-04-12 20:16:10 +0300

    navbar and footer responsive

--- my-project/src/components/Header/Header.jsx
+++ my-project/src/components/Header/Header.jsx
@@ -1,35 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BiCart } from 'react-icons/bi'
-import { BsSearch } from 'react-icons/bs'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { BiCart } from 'react-icons/bi';
 import {
   GiClothes,
   GiNecklace,
   GiWoodenChair,
   GiSoap
-} from 'react-icons/gi'
-import classes from "./Header.module.css"
+} from 'react-icons/gi';
+import classes from "./Header.module.css";
 
 const navItems = [
   { icon: GiClothes, text: "Traditional Clothes" },
   { icon: GiNecklace, text: "Handmade Accessories" },
   { icon: GiWoodenChair, text: "Home Decor" },
   { icon: GiSoap, text: "Skincare Products" }
-]
+];
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <section className={classes.fixed} style={{ backgroundColor: '#5a3921' }}>
+    <section className={classes.fixed}>
       <div className={classes.header__container}>
         {/* Logo */}
         <div className={classes.logo__container}>
           <Link to="/">
-            <h1 style={{ color: 'white' }}>EmpowerHer</h1>
+            <h1>EmpowerHer</h1>
           </Link>
         </div>
 
-        {/* Navigation Links */}
-        <ul className={classes.nav__items}>
+        {/* Desktop Navigation */}
+        <ul className={classes["nav__items--desktop"]}>
           {navItems.map((item, index) => (
             <li key={index} className={classes.nav__link}>
               <item.icon className={classes.nav__icon} />
@@ -38,22 +39,49 @@ function Header() {
           ))}
         </ul>
 
-        {/* Auth & Cart */}
-        <div className={classes.order__container}>
-          <Link to="/auth">
-            <div>
-              <p style={{ color: 'white' }}>Hello, Sign In</p>
-              <span style={{ color: 'white' }}>Account & Lists</span>
-            </div>
-          </Link>
-          <Link to="/cart" className={classes.cart}>
-            <BiCart size={35} style={{ color: 'white' }} />
-            <span style={{ color: 'white' }}>0</span>
-          </Link>
+        {/* Right Side Elements */}
+        <div className={classes.header__right}>
+          {/* Auth & Cart */}
+          <div className={classes.order__container}>
+            <Link to="/auth">
+              <div>
+                <p>Hello, Sign In</p>
+                <span>Account & Lists</span>
+              </div>
+            </Link>
+            <Link to="/cart" className={classes.cart}>
+              <BiCart size={35} />
+              <span>0</span>
+            </Link>
+          </div>
+
+          {/* Hamburger Menu (Mobile) */}
+          <button 
+            className={`${classes.hamburger} ${isMenuOpen ? classes.active : ''}`}
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <span className={classes.hamburger__line}></span>
+            <span className={classes.hamburger__line}></span>
+            <span className={classes.hamburger__line}></span>
+          </button>
         </div>
+
+        {/* Mobile Navigation */}
+        <ul className={`${classes["nav__items--mobile"]} ${isMenuOpen ? classes.active : ''}`}>
+          {navItems.map((item, index) => (
+            <li 
+              key={index} 
+              className={classes.nav__link}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              <item.icon className={classes.nav__icon} />
+              <span>{item.text}</span>
+            </li>
+          ))}
+        </ul>
       </div>
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- my-project/src/components/footer/footer.jsx
+++ my-project/src/components/footer/footer.jsx
@@ -7,60 +7,54 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer>
-      <section className={classes.footer_outer_container}>
-        <div className={classes.footer_inner_container}>
-          <div className={classes.first_section}>
-            <ul>
-            <div className={classes.logo__container}>
-          <Link to="/">
-            <h1 style={{ color: 'white' }}>EmpowerHer</h1>
-          </Link>
+    <footer className={classes.footer_outer_container}>
+      <div className={classes.footer_inner_container}>
+        {/* First Section */}
+        <div className={classes.first_section}>
+          <div className={classes.logo__container}>
+            <Link to="/">
+              <h1>EmpowerHer</h1>
+            </Link>
+          </div>
+          <ul className={classes.footer_icons}>
+            <li>
+              <a href="https://www.facebook.com/evangaditech" target="_blank" rel="noreferrer">
+                <LuFacebook />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.instagram.com/evangaditech/" target="_blank" rel="noreferrer">
+                <FaInstagram />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.youtube.com/@EvangadiTech" target="_blank" rel="noreferrer">
+                <AiOutlineYoutube />
+              </a>
+            </li>
+          </ul>
         </div>
 
-              <div className={classes.footer_icons}>
-                <li>
-                  <a href="https://www.facebook.com/evangaditech" target="_blank">
-                    <LuFacebook />
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.instagram.com/evangaditech/" target='_blank'>
-                    <FaInstagram />
-                  </a>
-                </li>
-                <li>
-                  <a href="https://www.youtube.com/@EvangadiTech" target='_blank'>
-                    <AiOutlineYoutube />
-                  </a>
-                </li>
-              </div>
-            </ul>
-          </div>
-
-          <div className={classes.second_section}>
-            <ul>
-              <h3>Useful Links</h3>
-              <div className={classes.lists}>
-                <li>How it works</li>
-                <li>Terms of Service</li>
-                <li>Privacy Policy</li>
-              </div>
-            </ul>
-          </div>
-
-          <div className={classes.third_section}>
-            <ul>
-              <h3>Contact Info</h3>
-              <div className={classes.lists}>
-                <li>Evangadi Networks</li>
-                <li>support@evangadi.com</li>
-                <li>+1-202-386-2702</li>
-              </div>
-            </ul>
-          </div>
+        {/* Second Section */}
+        <div className={classes.second_section}>
+          <h3>Useful Links</h3>
+          <ul className={classes.lists}>
+            <li>How it works</li>
+            <li>Terms of Service</li>
+            <li>Privacy Policy</li>
+          </ul>
         </div>
-      </section>
+
+        {/* Third Section */}
+        <div className={classes.third_section}>
+          <h3>Contact Info</h3>
+          <ul className={classes.lists}>
+            <li>Evangadi Networks</li>
+            <li>support@evangadi.com</li>
+            <li>+1-202-386-2702</li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 }
